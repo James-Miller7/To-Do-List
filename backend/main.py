74c6a0616a467c33863c12b3e4318b74605a90cd
James C Miller <james.miller.1363@gmail.com>
2025-06-18 16:58:05 -0400

    Finished endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String
+from fastapi import FastAPI, Depends, HTTPException, Query
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel
-
+from typing import Optional
 
 app = FastAPI()
 
@@ -17,6 +17,7 @@ class Item(Base):
   id = Column(Integer, primary_key=True, index=True)
   name = Column(String, index=True)
   description = Column(String)
+  complete = Column(Boolean, default=False, nullable=False)
 
 
 class ItemCreate(BaseModel):
@@ -27,6 +28,13 @@ class ItemResponse(BaseModel):
    id: int
    name: str
    description: str  
+   complete: bool
+
+   model_config = {"from_attributes": True}
+
+class ItemPatch(BaseModel):
+   name: Optional[str] = None
+   description: Optional[str] = None
 
 Base.metadata.create_all(bind=engine)
 
@@ -45,3 +53,55 @@ async def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_item)
     return db_item
+
+@app.get("/items/{item_id}", response_model=ItemResponse)
+async def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item is None:
+      raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@app.get("/items/", response_model=list[ItemResponse])
+async def read_all_items(complete: Optional[bool] = Query(None), name: Optional[str] = Query(None), db: Session = Depends(get_db)):
+   query = db.query(Item)
+   if complete is not None:
+      query = query.filter(Item.complete == complete)
+   if name is not None:
+      query = query.filter(Item.name == name)
+   return query
+
+@app.delete("/items/{item_id}", response_model=ItemResponse)
+async def delete_item(item_id: int, db: Session = Depends(get_db)):
+   db_item = db.query(Item).filter(Item.id == item_id).first()
+   if db_item is None:
+      raise HTTPException(status_code=404, detail="Item not found")
+   db.delete(db_item)
+   db.commit()
+   return db_item
+
+@app.patch("/items/{item_id}", response_model=ItemResponse)
+async def patch_item(item: ItemPatch, item_id: int, db: Session = Depends(get_db)):
+   db_item = db.query(Item).filter(Item.id == item_id).first()
+   if db_item is None:
+      raise HTTPException(status_code=404, detail="Item not found")
+   if item.description:
+    db_item.description = item.description
+   if item.name:
+    db_item.name = item.name
+
+   db.commit()
+   db.refresh(db_item)
+
+   return db_item
+
+@app.patch("/items/{item_id}/complete", response_model=ItemResponse)
+async def reverse_status(item_id: int, db: Session = Depends(get_db)):
+   db_item = db.query(Item).filter(Item.id == item_id).first()
+   if db_item is None:
+    raise HTTPException(status_code=404, detail="Item not found")
+   db_item.complete = not db_item.complete
+
+   db.commit()
+   db.refresh(db_item)
+
+   return db_item
